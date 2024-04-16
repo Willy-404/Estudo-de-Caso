e90a01f37eac8b398580dcadc48fe54d15510490
Willian dos Anjos <Willian2006.com@gmail.com>
2024-04-16 09:05:55 -0300

    IN THE BEGINNING
    Willian created the Classes and the Attributes, but the Main was empty and without codes

---
+++ estudo-de-caso/src/osmeninojatacopacote/Equipamento.java
@@ -0,0 +1,12 @@
+package osmeninojatacopacote;
+
+public class Equipamento {
+	private Integer codigo;
+	private String nome;
+	private String marca;
+	private Long preco;
+	private String datafab;
+	private Integer numerodeserie;
+	
+	
+}
