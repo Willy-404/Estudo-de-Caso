89665fcca09891d9ef2fa774e0e5f4b98217b18c
Willy404 <willian2006.com@gmail.com>
2024-04-24 14:06:30 -0300

    Getters e Setters ai

--- estudo-de-caso/src/osmeninojatacopacote/Equipamento.java
+++ estudo-de-caso/src/osmeninojatacopacote/Equipamento.java
@@ -7,4 +7,43 @@ public class Equipamento {
 	private Long preco;
 	private String datafab;
 	private Integer numerodeserie;
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public Long getPreco() {
+		return preco;
+	}
+	public void setPreco(Long preco) {
+		this.preco = preco;
+	}
+	public String getDatafab() {
+		return datafab;
+	}
+	public void setDatafab(String datafab) {
+		this.datafab = datafab;
+	}
+	public Integer getNumerodeserie() {
+		return numerodeserie;
+	}
+	public void setNumerodeserie(Integer numerodeserie) {
+		this.numerodeserie = numerodeserie;
+	}
+	
+	
 }
