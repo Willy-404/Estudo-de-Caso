851b7553fdd6e0c9c9c6cddadf36ebd86c32fe00
pedro <ph1953785@gmail.com>
2024-05-03 08:51:38 -0300

    As Coisa Predo

--- estudo-de-caso/src/osmeninojatacopacote/MainEstudodeGirandir.java
+++ estudo-de-caso/src/osmeninojatacopacote/MainEstudodeGirandir.java
@@ -30,43 +30,47 @@ public class MainEstudodeGirandir {
 			case 1:
 				//provavelmente teremos de mudar a ordem para ajeitar a verificação, ex: codigo 1°
 				Equipamento equipamento1 = new Equipamento();
-				System.out.println("Digite o nome do produto: ");
-				String nomedoequipamento = leitura.nextLine();
-				equipamento1.setNome(nomedoequipamento);
-				//verificação se tiver a mesma marca?
-				
-				System.out.println("Digite o numero de serie do produto: ");
-				String numseriedoequipamento = leitura.nextLine();
-				int numseriedoequipamento1 = Integer.valueOf(numseriedoequipamento);
-				equipamento1.setNumerodeserie(numseriedoequipamento1);
-				//verificação se tiver o mesmo nome e marca?
-				
 				System.out.println("Digite o codigo do produto: ");
 				String codigodoequipamento = leitura.nextLine();
 				int codigodoequipamento1 = Integer.valueOf(codigodoequipamento);
 				equipamento1.setCodigo(codigodoequipamento1);
 				for (Equipamento equipamento : estoque) {
-					if (codigodoequipamento1==(equipamento.getCodigo())) {
-						System.out.println("Codigo ja utilizado, digite outro");
-						//falta terminar a verificação
+				while(codigodoequipamento1==(equipamento.getCodigo())) {
+						System.out.println("Codigo ja utilizado, digite outro: ");
+					String codigoequipamentoconf=leitura.nextLine();
+					codigodoequipamento1 = Integer.valueOf(codigoequipamentoconf);
+					equipamento1.setCodigo(codigodoequipamento1);
 					}
 				}
-
+				
+				System.out.println("Digite o numero de serie do produto: ");
+				String numseriedoequipamento = leitura.nextLine();
+				int numseriedoequipamento1 = Integer.valueOf(numseriedoequipamento);
+				equipamento1.setNumerodeserie(numseriedoequipamento1);
+				while (numseriedoequipamento1<0) {
+					System.out.println("Numero de serie invalido, digite de novo: ");
+					String numserieconferir=leitura.nextLine();
+					numseriedoequipamento1=Integer.valueOf(numserieconferir);
+				}
+				System.out.println("Digite o nome do produto: ");
+				String nomedoequipamento = leitura.nextLine();
+				equipamento1.setNome(nomedoequipamento);
+											
 				System.out.println("Digite a marca: ");
 				String marcadoequipamento = leitura.nextLine();
 				equipamento1.setMarca(marcadoequipamento);
+				
+				System.out.println("Digite a data de fabricacao do produto: ");
+				String datafabdoequipamento = leitura.nextLine();
+				equipamento1.setDatafab(datafabdoequipamento);
+				estoque.add(equipamento1);
 				
 				System.out.println("Digite o preco do produto: ");
 				String precodoequipamento = leitura.nextLine();
 				long precodoequipamento1 = Long.valueOf(precodoequipamento);
 				equipamento1.setPreco(precodoequipamento1);
-				//verifica se o preço não é negativo
+				
 
-				System.out.println("Digite a data de fabricacao do produto: ");
-				String datafabdoequipamento = leitura.nextLine();
-				equipamento1.setDatafab(datafabdoequipamento);
-				estoque.add(equipamento1);
-				//não acho que precisa verificar data
 				break;
 
 			case 2:
@@ -120,38 +124,38 @@ public class MainEstudodeGirandir {
 						int po = Integer.valueOf(leitura.nextLine());
 						switch(po) {
 						case 1:
-							System.out.println("Renomeie o Equipamento:");
+							System.out.println("Renomeie o Equipamento: ");
 							String NomeParaSubstituir=leitura.nextLine();
 							equipamento.setNome(NomeParaSubstituir);
-							System.out.println("Nome alterado com sucesso");
+							System.out.println("Nome alterado com sucesso!");
 							po=0;
 							break;
 						case 2:
-							System.out.println("Digite um novo Numero de Serie");
+							System.out.println("Digite um novo Numero de Serie: ");
 							int NumSerieSubstituir=Integer.valueOf(leitura.nextLine());
 							equipamento.setNumerodeserie(NumSerieSubstituir);
-							System.out.println("Numero de Serie alterado com sucesso");
+							System.out.println("Numero de Serie alterado com sucesso!");
 							po=0;
 							break;
 						case 3:
-							System.out.println("Digite a nova marca do equipamento");
+							System.out.println("Digite a nova marca do equipamento: ");
 							String MarcaParaSubstituir=leitura.nextLine();
 							equipamento.setMarca(MarcaParaSubstituir);
-							System.out.println("Marca alterada com sucesso");
+							System.out.println("Marca alterada com sucesso!");
 							po=0;
 							break;
 						case 4:
-							System.out.println("Digite um novo Preco para o Equipamento");
+							System.out.println("Digite um novo Preco para o Equipamento: ");
 							long PrecoSubstituir=Integer.valueOf(leitura.nextLine());
 							equipamento.setPreco(PrecoSubstituir);
-							System.out.println("Preco alterado com sucesso");
+							System.out.println("Preco alterado com sucesso!");
 							po=0;
 							break;
 						case 5:
-							System.out.println("Digite a data de fabricacao do Equipamento:");
+							System.out.println("Digite a data de fabricacao do Equipamento: ");
 							String DataSubstituir=leitura.nextLine();
 							equipamento.setDatafab(DataSubstituir);
-							System.out.println("Data alterada com sucesso");
+							System.out.println("Data alterada com sucesso!");
 							po=0;
 							break;
 						}while (po!=0);
